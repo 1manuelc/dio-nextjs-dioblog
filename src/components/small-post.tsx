faa68a32dd7aa0332888858fae7e65cbb3366e43
1manuelc <manuelwn21@gmail.com>
2024-12-26 14:50:03 -0300

    feat: add loading spinner component and update SmallPost to include user and post IDs for navigation

--- src/components/small-post.tsx
+++ src/components/small-post.tsx
@@ -1,13 +1,20 @@
 interface SmallPostProps {
 	title: string;
 	description?: string;
+	userId: string;
+	postId: string;
 }
 
-const SmallPost: React.FC<SmallPostProps> = ({ title, description }) => {
+const SmallPost: React.FC<SmallPostProps> = ({
+	title,
+	description,
+	userId,
+	postId,
+}) => {
 	return (
-		<li className="w-full border border-zinc-800 hover:border-zinc-600 rounded-lg py-2 px-3 transition-all">
-			<a href="none">
-				<h4 className="font-bold text-lg">{title}</h4>
+		<li className='w-full border border-zinc-800 hover:border-zinc-600 rounded-lg py-2 px-3 transition-all'>
+			<a href={`/user/${userId}/post/${postId}`}>
+				<h4 className='font-bold text-lg'>{title}</h4>
 				<p
 					className={
 						description?.length !== 0 ? 'text-zinc-400' : 'text-zinc-600'
